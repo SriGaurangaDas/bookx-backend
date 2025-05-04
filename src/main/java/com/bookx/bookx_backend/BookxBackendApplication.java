473aea98ea761d289c2f1ce3a3a39dc2bce001fa
SriGaurangaDas <pranabbabu.nandi@gmail.com>
2025-05-04 18:07:57 +0530

    chore: Initial Spring Boot project scaffold

---
+++ src/main/java/com/bookx/bookx_backend/BookxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookx.bookx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookxBackendApplication.class, args);
+	}
+
+}
